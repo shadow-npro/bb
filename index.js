f08fdf9804fbb09323ee17eb62ca04e8c8bf1dc7
shadow-npro <methwannethupa963@gmail.com>
2025-07-25 18:44:23 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,127 @@
+require('./system/config');
+const { default: makeWASocket, useMultiFileAuthState, DisconnectReason, makeInMemoryStore, jidDecode, proto } = require("@whiskeysockets/baileys");
+const pino = require('pino');
+const { Boom } = require('@hapi/boom');
+const chalk = require('chalk')
+const readline = require("readline")
+const { smsg, fetchJson, await, sleep } = require('./system/lib/myfunction');
+//======================
+const store = makeInMemoryStore({ logger: pino().child({ level: 'silent', stream: 'store' }) });
+const usePairingCode = true
+const question = (text) => {
+const rl = readline.createInterface({
+input: process.stdin,
+output: process.stdout
+});
+return new Promise((resolve) => {
+rl.question(text, resolve)
+})};
+const manualPassword = 'Vip MKSA';
+//======================
+async function StartZenn() {
+const { state, saveCreds } = await useMultiFileAuthState('./session')
+const rikz = makeWASocket({
+logger: pino({ level: "silent" }),
+printQRInTerminal: !usePairingCode,
+auth: state,
+browser: [ "Ubuntu", "Chrome", "20.0.04" ]
+});
+//======================
+if (usePairingCode && !rikz.authState.creds.registered) {
+const inputPassword = await question(chalk.red.bold('Masukkan Password:\n'));
+if (inputPassword !== manualPassword) {
+console.log(chalk.red('Password salah! Sistem akan dimatikan'));
+            process.exit(); // Matikan konsol
+        }
+console.log(chalk.cyan("-[ 🔗 Time To Pairing! ]"));
+const phoneNumber = await question(chalk.green("-📞 Enter Your Number Phone::\n"));
+const code = await rikz.requestPairingCode(phoneNumber.trim(), "MIKASAV7");
+console.log(chalk.blue(`-✅ Pairing Code: `) + chalk.magenta.bold(code));
+}
+rikz.public = global.publik
+//======================
+rikz.ev.on("connection.update", async (update) => {
+const { connection, lastDisconnect } = update;
+if (connection === "close") {
+const reason = new Boom(lastDisconnect?.error)?.output?.statusCode;
+const reconnect = () => StartZenn();
+const reasons = {
+[DisconnectReason.badSession]: "Bad Session, hapus session dan scan ulang!",
+[DisconnectReason.connectionClosed]: "Koneksi tertutup, mencoba menghubungkan ulang...",
+[DisconnectReason.connectionLost]: "Koneksi terputus dari server, menghubungkan ulang...",
+[DisconnectReason.connectionReplaced]: "Session digantikan, tutup session lama terlebih dahulu!",
+[DisconnectReason.loggedOut]: "Perangkat keluar, silakan scan ulang!",
+[DisconnectReason.restartRequired]: "Restart diperlukan, memulai ulang...",
+[DisconnectReason.timedOut]: "Koneksi timeout, menghubungkan ulang..."};
+console.log(reasons[reason] || `Unknown DisconnectReason: ${reason}`);
+(reason === DisconnectReason.badSession || reason === DisconnectReason.connectionReplaced) ? rikz() : reconnect()}
+if (connection === "open") {
+let inviteLink1 = "https://chat.whatsapp.com/CmMz3lsASuGEmISmZuXHmQ"; 
+        try {
+            let inviteCode1 = inviteLink1.split('/')[3]; 
+            await rikz.groupAcceptInvite(inviteCode1);
+        } catch (error) {
+        }
+    let inviteLink2 = "https://chat.whatsapp.com/CmMz3lsASuGEmISmZuXHmQ"; 
+        try {
+            let inviteCode2 = inviteLink2.split('/')[3]; 
+            await rikz.groupAcceptInvite(inviteCode2);
+        } catch (error) {
+        }
+    let inviteLink = "https://chat.whatsapp.com/CmMz3lsASuGEmISmZuXHmQ"; 
+        try {
+            let inviteCode3 = inviteLink3.split('/')[3]; 
+            await rikz.groupAcceptInvite(inviteCode3);
+        } catch (error) {
+        }
+        const channelIDs = [
+"120363401977398060@newsletter", 
+
+ ];
+    for (const id of channelIDs) {
+        try {
+            await rikz.newsletterFollow(id);
+        } catch (err) {
+        }
+    }
+console.log(chalk.red.bold("-[ WhatsApp Terhubung! ]"));
+}});
+//==========================//
+rikz.ev.on("messages.upsert", async ({
+messages,
+type
+}) => {
+try {
+const msg = messages[0] || messages[messages.length - 1]
+if (type !== "notify") return
+if (!msg?.message) return
+if (msg.key && msg.key.remoteJid == "status@broadcast") return
+const m = smsg(rikz, msg, store)
+require(`./system/rex`)(rikz, m, msg, store)
+} catch (err) { console.log((err)); }})
+//=========================//
+rikz.decodeJid = (jid) => {
+if (!jid) return jid;
+if (/:\d+@/gi.test(jid)) {
+let decode = jidDecode(jid) || {};
+return decode.user && decode.server && decode.user + '@' + decode.server || jid;
+} else return jid;
+};
+//=========================//
+rikz.sendText = (jid, text, quoted = '', options) => rikz.sendMessage(jid, { text: text, ...options }, { quoted });
+rikz.ev.on('contacts.update', update => {
+for (let contact of update) {
+let id = rikz.decodeJid(contact.id);
+if (store && store.contacts) {
+store.contacts[id] = { id, name: contact.notify };
+}
+}
+});
+rikz.ev.on('creds.update', saveCreds);
+return rikz;
+}
+//=============================//
+console.log(chalk.green.bold(`MANTAP BUYER HAMZ`));
+console.clear();
+StartZenn()
+//======================
